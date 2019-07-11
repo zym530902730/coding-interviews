545fec88138eb3f78324052e5868db7c150a1204
zym530902730 <530902730@qq.com>
2019-07-12 00:04:31 +0800

    Initial commit

---
+++ src/二维数组中的查找.java
@@ -0,0 +1,30 @@
+/**
+ * Title: 二维数组中的查找
+ * ProjectName: 剑指offer
+ * Function:  TODO
+ * author     Yiming Zhao
+ * Date:      2019-07-11 23:50
+ */
+public class 二维数组中的查找 {
+
+    public boolean Find(int target, int [][] array) {
+        if (array == null) {
+            return false;
+        }
+        // 初始化 行 列，定义为右上角
+        int rows = 0;
+        int columns = array[0].length - 1;
+        while (rows <= array.length - 1 && columns >= 0) {
+            if (array[rows][columns] == target) {
+                return true;
+            } else if (array[rows][columns] < target) {
+                rows++;
+            } else {
+                columns--;
+            }
+        }
+
+        return false;
+    }
+
+}
---
+++ src/数组中重复的数字.java
@@ -0,0 +1,31 @@
+/**
+ * Title: 数组中重复的数字
+ * ProjectName: 剑指offer
+ * Function:  TODO
+ * author     Yiming Zhao
+ * Date:      2019-06-06 00:21
+ */
+public class 数组中重复的数字 {
+    public boolean duplicate(int[] nums, int length, int[] duplication) {
+        if (nums == null || length <= 0) {
+            return false;
+        }
+        for (int i = 0; i < length; i++) {
+            while (nums[i] != i) {
+                if (nums[i] == nums[nums[i]]) {
+                    duplication[0] = nums[i];
+                    return true;
+                }
+                swap(nums, i, nums[i]);
+            }
+        }
+        return false;
+    }
+
+    private void swap ( int[] nums, int i, int j){
+        int tem = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tem;
+    }
+
+}
